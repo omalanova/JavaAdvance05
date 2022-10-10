c62143dbfc982fc1ffa07aa43dc7d83e59fad3f7
omalanova <oyuna_eles@mail.ru>
2022-10-10 23:21:24 +0800

    commit hw2 task2

--- src/HW/hw2.java
+++ src/HW/hw2.java
@@ -1,16 +1,24 @@
 package HW;
 
 public class hw2 {
-    //task1
     public static void main (String args[]) {
+
+        //task1
         int a = 5;
-        int b = 5;
+        int b = 6;
         if (a == b) {
             System.out.println(a + " == " + b);
         } else if (a > b){
             System.out.println(a + " > " + b);
         } else {
             System.out.println(a + " < " + b);
+        }
+
+        //task2
+        if ((a + b) % 2 == 0) {
+            System.out.println("maybe a and b are even");
+        } else {
+            System.out.println("some variable is odd");
         }
     }
 }
