376e45db0fa2f804f7cf68be5acbe8b3b9827883
omalanova <oyuna_eles@mail.ru>
2022-10-10 22:21:53 +0800

    commit task1

--- src/HW/hw1.java
+++ src/HW/hw1.java
@@ -1,5 +1,10 @@
 package HW;
 
 public class hw1 {
-
+    public static void main(String[] args) {
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Поехали!!!");
+    }
 }
