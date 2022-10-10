9364fe5ca4aee67d241c0de446d71cb1806560a8
omalanova <oyuna_eles@mail.ru>
2022-10-10 23:14:56 +0800

    commit hw2 task1

--- src/HW/hw2.java
+++ src/HW/hw2.java
@@ -1,4 +1,16 @@
 package HW;
 
 public class hw2 {
+    //task1
+    public static void main (String args[]) {
+        int a = 5;
+        int b = 5;
+        if (a == b) {
+            System.out.println(a + " == " + b);
+        } else if (a > b){
+            System.out.println(a + " > " + b);
+        } else {
+            System.out.println(a + " < " + b);
+        }
+    }
 }
