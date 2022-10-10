6f5638216b5afd6ea6e9208f4c2defb41ca2eed5
omalanova <oyuna_eles@mail.ru>
2022-10-11 00:08:14 +0800

    commit hw3 task1-5

---
+++ src/HW/hw3.java
@@ -0,0 +1,82 @@
+package HW;
+
+public class hw3 {
+    public static void main(String[] args) {
+        /* Задача №1
+
+Дан массив:
+int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+необходимо вывести сумму всех значений массива.
+*/
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        int s = 0;
+        for (int i = 0; i < array.length; i++) {
+            s += array[i];
+        }
+        System.out.println("s = " + s);
+
+        /*
+        Задача №2
+
+Дан массив:
+int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+необходимо вывести максимальное значение массива.
+
+         */
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("max = " + max);
+
+        /*
+        Задача №3
+
+Дан массив:
+int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+необходимо вывести минимальное значение массива.
+
+         */
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("min = " + min);
+
+        /*
+        Задача №4
+
+Дан массив:
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+необходимо вывести среднее арифметическое всех значений массива.
+
+         */
+        int s1 = 0;
+        int[] array2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < array2.length; i++) {
+            s1 += array2[i];
+        }
+        System.out.println("average = " + s1 / array2.length);
+
+        /*
+        Задача №5
+
+Дан массив:
+int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+необходимо вывести сумму элементов массива.
+
+         */
+        int[][] array3 = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+        int s2 = 0;
+        for (int i = 0; i < array3.length; i++) {
+            for (int j = 0; j < array3[i].length; j++) {
+                s2 += array3[i][j];
+            }
+        }
+        System.out.println("s2 = " + s2);
+    }
+}
