435ce523a3fdf2a28ae0035fdf20f26ba5d54aba
omalanova <oyuna_eles@mail.ru>
2022-10-11 22:03:51 +0800

    commit hw4 task2

--- src/HW/hw4.java
+++ src/HW/hw4.java
@@ -24,5 +24,30 @@ public class hw4 {
             System.out.println();
         }
 
+        /*
+        Задача №2
+
+0  1  2  3  4  5  6  7  8  9
+   0  1  2  3  4  5  6  7  8
+      0  1  2  3  4  5  6  7
+         0  1  2  3  4  5  6
+            0  1  2  3  4  5
+               0  1  2  3  4
+                  0  1  2  3
+                     0  1  2
+                        0  1
+                           0
+
+         */
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i > j) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
