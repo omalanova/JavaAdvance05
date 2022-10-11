ce320a7150317e529bdb517db58527cbbc5241a4
omalanova <oyuna_eles@mail.ru>
2022-10-11 21:47:55 +0800

    commit hw4 task1

---
+++ src/HW/hw4.java
@@ -0,0 +1,28 @@
+package HW;
+
+public class hw4 {
+    public static void main (String args[]) {
+        /*
+        Задача №1
+
+0  1  2  3  4  5  6  7  8  9
+0  1  2  3  4  5  6  7  8
+0  1  2  3  4  5  6  7
+0  1  2  3  4  5  6
+0  1  2  3  4  5
+0  1  2  3  4
+0  1  2  3
+0  1  2
+0  1
+0
+
+         */
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10 - i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
