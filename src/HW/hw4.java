b2689f040629b26bf906e7dc6f81d7b52b1cfd9c
omalanova <oyuna_eles@mail.ru>
2022-10-12 21:51:09 +0800

    commit hw4 task3

--- src/HW/hw4.java
+++ src/HW/hw4.java
@@ -44,8 +44,37 @@ public class hw4 {
                 if (i > j) {
                     System.out.print("  ");
                 } else {
-                    System.out.print(j + " ");
+                    System.out.print((j - i) + " ");
                 }
+            }
+            System.out.println();
+        }
+
+        /*
+        Задача №3
+
+9 8 7 6 5 4 3 2 1 0 1 2 3 4 5 6 7 8 9
+  8 7 6 5 4 3 2 1 0 1 2 3 4 5 6 7 8
+    7 6 5 4 3 2 1 0 1 2 3 4 5 6 7
+      6 5 4 3 2 1 0 1 2 3 4 5 6
+        5 4 3 2 1 0 1 2 3 4 5
+          4 3 2 1 0 1 2 3 4
+            3 2 1 0 1 2 3
+              2 1 0 1 2
+                1 0 1
+                  0
+
+         */
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i > j) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print((9 - j) + " ");
+                }
+            }
+            for (int j = 1; j < 10 - i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
